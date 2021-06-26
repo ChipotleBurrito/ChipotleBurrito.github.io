1dd116a4493ec97f83a8fa4cd889ef06d5f00ddd
KrishArora100 <85039577+KrishArora100@users.noreply.github.com>
2021-06-26 16:48:28 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,25 +230,24 @@
   });
   
   /* Drag and Drop */
-// dragover and dragenter events need to have 'preventDefault' called
-// in order for the 'drop' event to register. 
-// See: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Drag_operations#droptargets
-dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
-  evt.preventDefault();
-};
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
 
-dropContainer.ondrop = function(evt) {
-  // pretty simple -- but not for IE :(
-  fileInput.files = evt.dataTransfer.files;
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
 
-  // If you want to use some of the dropped files
-  const dT = new DataTransfer();
-  dT.items.add(evt.dataTransfer.files[0]);
-  dT.items.add(evt.dataTransfer.files[3]);
-  fileInput.files = dT.files;
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
 
-  evt.preventDefault();
-};
 
 
   
